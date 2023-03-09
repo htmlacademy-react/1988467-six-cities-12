0d5f458c33f5f0b9aec1d939f58fa5239c7e6c74
Julia Antoshkina <julia.antoshkina@gmail.com>
2023-03-09 01:44:08 +0100

    Added 404-page and routes

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,4 +1,9 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from '../../pages/main-page/main-page';
+import NotFoundPage from '../../pages/not-found-page/not-found-page';
+import LoginPage from '../../pages/login-page/login-page';
+import FavoritesPage from '../../pages/favorites-page/favorites-page';
+import OfferPage from '../../pages/offer-page/offer-page';
 
 type AppScreenProps = {
   rentalOffersCount: number;
@@ -6,7 +11,19 @@ type AppScreenProps = {
 }
 
 function App({ rentalOffersCount, placeCardCount }: AppScreenProps): JSX.Element {
-  return <MainPage rentalOffersCount={rentalOffersCount} placeCardCount={placeCardCount} />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/'>
+          <Route index element={<MainPage rentalOffersCount={rentalOffersCount} placeCardCount={placeCardCount} />} />
+          <Route path='login' element={<LoginPage />} />
+          <Route path='favorites' element={<FavoritesPage />} />
+          <Route path='offer/:id' element={<OfferPage />} />
+        </Route>
+        <Route path='*' element={<NotFoundPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
