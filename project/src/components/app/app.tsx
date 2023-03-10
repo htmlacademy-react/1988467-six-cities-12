c13079fe78e3b8ba98c81fdd2462e05fc3d623fc
Julia Antoshkina <julia.antoshkina@gmail.com>
2023-03-10 23:28:52 +0100

    Added logo, private route, helmetProvider, moved consts to const.ts

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -4,6 +4,9 @@ import NotFoundPage from '../../pages/not-found-page/not-found-page';
 import LoginPage from '../../pages/login-page/login-page';
 import FavoritesPage from '../../pages/favorites-page/favorites-page';
 import OfferPage from '../../pages/offer-page/offer-page';
+import { AppRoute, AuthorizationStatus } from '../../const';
+import PrivateRoute from '../private-route/private-route';
+import { HelmetProvider } from 'react-helmet-async';
 
 type AppScreenProps = {
   rentalOffersCount: number;
@@ -12,17 +15,25 @@ type AppScreenProps = {
 
 function App({ rentalOffersCount, placeCardCount }: AppScreenProps): JSX.Element {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/'>
-          <Route index element={<MainPage rentalOffersCount={rentalOffersCount} placeCardCount={placeCardCount} />} />
-          <Route path='login' element={<LoginPage />} />
-          <Route path='favorites' element={<FavoritesPage />} />
-          <Route path='offer/:id' element={<OfferPage />} />
-        </Route>
-        <Route path='*' element={<NotFoundPage />} />
-      </Routes>
-    </BrowserRouter>
+    <HelmetProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path={AppRoute.Main}>
+            <Route index element={<MainPage rentalOffersCount={rentalOffersCount} placeCardCount={placeCardCount} />} />
+            <Route path={AppRoute.Login} element={<LoginPage />} />
+            <Route path={AppRoute.Favorites}
+              element={
+                <PrivateRoute authorizationStatus={AuthorizationStatus.NoAuth}>
+                  <FavoritesPage />
+                </PrivateRoute>
+              }
+            />
+            <Route path={AppRoute.Room} element={<OfferPage />} />
+          </Route>
+          <Route path='*' element={<NotFoundPage />} />
+        </Routes>
+      </BrowserRouter>
+    </HelmetProvider>
   );
 }
 
--- project/src/index.tsx
+++ project/src/index.tsx
@@ -1,14 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
-
-const RENTAL_OFFERS_COUNT = 312;
-const PLACE_CARD_COUNT = [1, 2, 3, 4, 5];
-
-const RentalInfo = {
-  RentalOffersCount: RENTAL_OFFERS_COUNT,
-  PlaceCardCount: PLACE_CARD_COUNT,
-} as const;
+import { RentalInfo } from './const';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
